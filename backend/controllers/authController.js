ec1a80974047c2b2c8a3292dbaafda5078d8945f
URVIL2512 <urvilsolanki2004@gmail.com>
2026-01-16 19:02:59 +0530

    Add phone number display in Profile modal and login response

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -60,6 +60,7 @@ export const login = async (req, res) => {
         _id: user._id,
         name: user.name,
         email: user.email,
+        phone: user.phone,
         role: user.role,
         token: generateToken(user._id),
       });
